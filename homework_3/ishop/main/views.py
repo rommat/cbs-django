c314339217b574396594b576cc47fd51604e20f6
Ro.Ma <roman.matviichuk@ukr.net>
2024-11-25 16:26:13 +0200

    Add main view

--- homework_3/ishop/main/views.py
+++ homework_3/ishop/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+
 
 # Create your views here.
+def main(request: HttpRequest):
+    return render(request, 'main/base.html')
