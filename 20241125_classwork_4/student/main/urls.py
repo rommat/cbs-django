d29ee31aaaf3a8866771b802fa77bcac5771700a
Ro.Ma <roman.matviichuk@ukr.net>
2024-11-27 16:48:34 +0200

    Add student grades page

--- 20241125_classwork_4/student/main/urls.py
+++ 20241125_classwork_4/student/main/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='homepage'),
+    path('students/<int:student_id>/', student_grades, name='studentgrades'),
 ]
--- 20241125_classwork_4/student/main/views.py
+++ 20241125_classwork_4/student/main/views.py
@@ -9,3 +9,15 @@ def index(request: HttpRequest):
     context = {'students': students}
 
     return render(request, 'main/index.html', context)
+
+
+def student_grades(request: HttpRequest, student_id: int):
+    student = Student.objects.get(id=student_id)
+    grades = student.grades.all()
+    context = {
+        'student': student,
+        'grades': grades,
+        'page_title': f'{student.first_name} {student.last_name}'
+    }
+
+    return render(request, 'main/grades.html', context)
